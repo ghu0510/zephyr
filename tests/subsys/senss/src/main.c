dc1b334eb5772c269309d412f4363dd328232625
Zhang Lixu <lixu.zhang@intel.com>
2022-09-26 15:36:47 +0800

    tests: subsys: senss: add test case for sensor api
    1. test case for get sensors
    2. test case for open/close sensor
    3. test case for set sensor interval
    
    Signed-off-by: Zhang Lixu <lixu.zhang@intel.com>

--- tests/subsys/senss/src/main.c
+++ tests/subsys/senss/src/main.c
@@ -6,9 +6,15 @@
 
 #include <zephyr/ztest.h>
 #include <zephyr/senss/senss.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys_clock.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(test_senss, LOG_LEVEL_INF);
+
+#define INTERVAL_10HZ (100 * USEC_PER_MSEC)
+#define INTERVAL_20HZ (50 * USEC_PER_MSEC)
+#define INVALID_INTERVAL_US 1 /* 1 us is smaller than the minimal interval */
 
 /**
  * @brief Senss Tests setup
@@ -35,3 +41,155 @@ static void teardown(void *ptr)
 }
 
 ZTEST_SUITE(senss_tests, NULL, setup, NULL, NULL, teardown);
+
+#define DT_DRV_COMPAT zephyr_senss
+#define DT_SENSOR_TYPE_INDEX(node)				\
+	{							\
+		.type = DT_PROP(node, sensor_type),		\
+		.index = DT_PROP_OR(node, sensor_index, 0),	\
+	}
+
+struct sensor_info_t {
+	int32_t type;
+	int32_t index;
+};
+
+static struct sensor_info_t sensors[] = {
+	DT_FOREACH_CHILD_SEP(DT_DRV_INST(0), DT_SENSOR_TYPE_INDEX, (,))
+};
+
+static bool lookup_sensor_in_dt(int32_t type, int32_t index)
+{
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(sensors); ++i) {
+		if (sensors[i].type == type &&
+				sensors[i].index == index) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+/**
+ * @brief Test Get Sensors
+ *
+ * This test verifies senss_get_sensors
+ */
+ZTEST(senss_tests, test_senss_get_sensors)
+{
+	const struct senss_sensor_info *infos;
+	int num = senss_get_sensors(&infos);
+	int i = 0;
+
+	/* Check the return value */
+	zassert_equal(num, ARRAY_SIZE(sensors), "Sensors num mismatch");
+	zassert_not_null(infos, "Sensors infos is null");
+
+	for (i = 0; i < num; ++i) {
+		/* Check the content */
+		zassert_true(lookup_sensor_in_dt(infos[i].type,
+					infos[i].sensor_index),
+				"sensor info mismatch");
+	}
+}
+
+#define INVALID_SENSOR_TYPE (-1)
+#define INVALID_SENSOR_INDEX (-1)
+
+/**
+ * @brief Test Open Sensor
+ *
+ * This test verifies senss_open_sensors
+ */
+ZTEST(senss_tests, test_senss_open_sensors)
+{
+	int handle = 0;
+	int ret;
+
+	/* positive test */
+	ret = senss_open_sensor(SENSS_SENSOR_TYPE_MOTION_ACCELEROMETER_3D, 0,
+			&handle);
+	zassert_equal(ret, 0, "Open ACC 0 failed");
+	zassert_not_equal(handle, SENSS_SENSOR_INVALID_HANDLE,
+			"Open ACC 0 failed");
+
+	ret = senss_open_sensor(SENSS_SENSOR_TYPE_MOTION_ACCELEROMETER_3D, 1,
+			&handle);
+	zassert_equal(ret, 0, "Open ACC 1 failed");
+	zassert_not_equal(handle, SENSS_SENSOR_INVALID_HANDLE,
+			"Open ACC 1 failed");
+
+	/* negative test */
+	ret = senss_open_sensor(INVALID_SENSOR_TYPE, 0, &handle);
+	zassert_true(ret < 0, "Negative test 2 failed");
+	zassert_equal(handle, SENSS_SENSOR_INVALID_HANDLE,
+			"Negative test 1 failed");
+
+	ret = senss_open_sensor(SENSS_SENSOR_TYPE_MOTION_ACCELEROMETER_3D,
+			INVALID_SENSOR_INDEX, &handle);
+	zassert_true(ret < 0, "Negative test 2 failed");
+	zassert_equal(handle, SENSS_SENSOR_INVALID_HANDLE,
+			"Negative test 2 failed");
+}
+
+/**
+ * @brief Test Close Sensor
+ *
+ * This test verifies senss_close_sensors
+ */
+ZTEST(senss_tests, test_senss_close_sensors)
+{
+	int handle = 0;
+	int ret;
+
+	/* positive test */
+	ret = senss_open_sensor(SENSS_SENSOR_TYPE_MOTION_ACCELEROMETER_3D, 0,
+			&handle);
+	zassert_equal(ret, 0, "Open ACC 0 failed");
+	zassert_not_equal(handle, SENSS_SENSOR_INVALID_HANDLE,
+			"Open ACC 0 failed");
+
+	ret = senss_close_sensor(handle);
+	zassert_equal(ret, 0, "Close ACC 0 failed");
+
+	/* negative test */
+	ret = senss_close_sensor(SENSS_SENSOR_INVALID_HANDLE);
+	zassert_true(ret < 0, "Negative test failed");
+}
+
+/**
+ * @brief Test Set Interval
+ *
+ * This test verifies senss_set_interval
+ */
+ZTEST(senss_tests, test_senss_set_interval)
+{
+	int handle = 0;
+	int ret;
+
+	ret = senss_open_sensor(SENSS_SENSOR_TYPE_MOTION_ACCELEROMETER_3D, 0,
+			&handle);
+	zassert_equal(ret, 0, "Open ACC 0 failed");
+	zassert_not_equal(handle, SENSS_SENSOR_INVALID_HANDLE,
+			"Open ACC 0 failed");
+
+	/* positive test */
+	ret = senss_set_interval(handle, INTERVAL_10HZ);
+	zassert_equal(ret, 0, "Set ACC 0 interval 100 ms failed");
+
+	ret = senss_set_interval(handle, INTERVAL_20HZ);
+	zassert_equal(ret, 0, "Set ACC 0 interval 50 ms failed");
+
+	/* negative test */
+	ret = senss_set_interval(handle, INVALID_INTERVAL_US);
+	zassert_true(ret < 0, "Negative test 1 failed");
+
+	ret = senss_set_interval(SENSS_SENSOR_INVALID_HANDLE,
+			INTERVAL_10HZ);
+	zassert_true(ret < 0, "Negative test 2 failed");
+
+	ret = senss_close_sensor(handle);
+	zassert_equal(ret, 0, "Close ACC 0 failed");
+}
